ad390fa0414e9c0b953e42f09c274e1218bcd15d
yoanslvy <97676023+yoanslvy@users.noreply.github.com>
2024-10-17 13:28:57 +0200

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Cometh core getting started example
+
+Discover all the cometh tools here: [our documentation](https://docs.cometh.io/connect-4337/).
+
+## Installation
+
+> Don't forget to add install bun package manager: [text](https://bun.sh/docs/installation)
+
+Clone the repo and run `bun install`
+
+## Start
+
+Setup your .env file from the .env.example file.
+
+```
+export NEXT_PUBLIC_COMETH_API_KEY=YOUR_API_KEY
+export NEXT_PUBLIC_4337_BUNDLER_URL="https://bundler.cometh.io/421614?apikey=YOUR_API_KEY"
+export NEXT_PUBLIC_4337_PAYMASTER_URL="https://paymaster.cometh.io/421614?apikey=YOUR_API_KEY"
+```
+
+After the successfull installation of the packages and the chain update, run: `bun dev`
---
+++ src/app/layout.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Inter } from "next/font/google";
+import "./lib/ui/globals.css";
+
+import { ConnectProvider } from "@cometh/connect-react-hooks";
+import { http, WagmiProvider, createConfig } from "wagmi";
+import * as chains from "viem/chains";
+import { extractChain } from "viem";
+
+const queryClient = new QueryClient();
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+const apiKey = process.env.NEXT_PUBLIC_COMETH_API_KEY;
+const bundlerUrl = process.env.NEXT_PUBLIC_4337_BUNDLER_URL;
+const paymasterUrl = process.env.NEXT_PUBLIC_4337_PAYMASTER_URL;
+
+const chainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID!);
+
+const chain = extractChain({
+  chains: Object.values(chains),
+  id: chainId,
+});
+
+if (!apiKey) throw new Error("API key not found");
+if (!bundlerUrl) throw new Error("Bundler Url not found");
+
+const config = createConfig({
+  chains: [chain],
+  transports: {
+    [chain.id]: http(),
+  },
+  ssr: true,
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <ConnectProvider
+            config={{
+              paymasterUrl,
+              bundlerUrl,
+              apiKey,
+              chain: chain,
+            }}
+            queryClient={queryClient}
+          >
+            <body className={inter.className}>{children}</body>
+          </ConnectProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </html>
+  );
+}
